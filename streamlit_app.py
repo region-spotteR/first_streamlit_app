c51cc5f01f97d9a7b695935e846d0ccd1a13f94b
region-spotteR <32107403+region-spotteR@users.noreply.github.com>
2023-07-05 10:08:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit
+
+streamlit.title("Some Dinner")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
